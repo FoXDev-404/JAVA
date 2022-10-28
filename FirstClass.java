5290a67e78825397feef1bb4bf614a2bca350dee
Rajpal Nishad <rajpal010304@gmail.com>
2022-10-29 03:45:03 +0530

    sahpe

--- FirstClass.java
+++ FirstClass.java
@@ -4,5 +4,37 @@ class FirstClass {
         System.out.println("Hello World!"); // println "ln"= Next line
         System.out.print("Hello World!\n"); // \n = Next line
         System.out.print("Hello World!"); // print = Smae line
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("*******");
+        System.out.println("********");
+        System.out.println("*********");
+        System.out.println("**********");
+        System.out.println("***********");
+        System.out.println("************");
+        System.out.println("*************");
+        System.out.println("**************");
+        System.out.println("***************");
+        System.out.println("****************");
+        System.out.println("***************");
+        System.out.println("**************");
+        System.out.println("*************");
+        System.out.println("************");
+        System.out.println("***********");
+        System.out.println("**********");
+        System.out.println("*********");
+        System.out.println("********");
+        System.out.println("*******");
+        System.out.println("******");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
