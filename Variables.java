e51bd8e988ef812bc99226b0050d3e915d3132b2
Rajpal Nishad <rajpal010304@gmail.com>
2022-11-04 20:12:20 +0530

    This is an example of Variables.

---
+++ Variables.java
@@ -0,0 +1,26 @@
+public class Variables {
+    // This is an example of Variables.
+    public static void main(String[] args) {
+        // Variables.
+        String name = "Rajpal Nishad";
+        int age = 18;
+        double cgpa = 9.1;
+        int a = 20;
+        int b = 15;
+
+        /*
+         * a, b, name, age, cgpa are the variables,
+         * we can change the values without any errors.
+         */
+        a = 25;
+        System.out.println(a);
+        b = 20;
+        System.out.println(b);
+        name = "Unknown";
+        System.out.println(name);
+        age = 19;
+        System.out.println(age);
+        cgpa = 9.2;
+        System.out.println(cgpa);
+    }
+}
