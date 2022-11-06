c762d6c10cdc83f304db099d6b33e5e709517555
Rajpal Nishad <rajpal010304@gmail.com>
2022-11-06 22:34:45 +0530

    Input

---
+++ Input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args) {
+        // Input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        // nextInt() for Integer.
+        // nextFloat for floating point value.
+        // more & more.
+        System.out.println(name);
+    }
+
+}
