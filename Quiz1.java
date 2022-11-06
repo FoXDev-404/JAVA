456fa8fa9473f526bd5dc9a2e48da5a81464cee4
Rajpal Nishad <rajpal010304@gmail.com>
2022-11-06 22:15:33 +0530

    To Calculate : (a*b)/(a-b) if a=10, b=5.

---
+++ Quiz1.java
@@ -0,0 +1,11 @@
+public class Quiz1 {
+    public static void main(String[] args) {
+
+        // To Calculate : (a*b)/(a-b) if a=10, b=5.
+        int a = 10;
+        int b = 5;
+        int cal = (a * b) / (a - b);
+        System.out.println(cal);
+
+    }
+}
