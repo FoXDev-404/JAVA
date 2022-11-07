963e6c369058591c17b2e07bde813fcb1815a53e
Rajpal Nishad <rajpal010304@gmail.com>
2022-11-07 22:17:55 +0530

    This is an example of conditional statement if-else, else if & input.

---
+++ Conditions1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Conditions1 {
+    public static void main(String[] args) {
+        // This is an example of conditional statement if-else, else if & input.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x :");
+        int x = sc.nextInt();
+        System.out.print("Enter y :");
+        int y = sc.nextInt();
+        if (x > y) {
+            System.out.println("X > Y " + x + ">" + y);
+        } else if (x == y) {
+            System.out.println("X = Y " + x + "=" + y);
+        } else {
+            System.out.println("X < Y " + x + "<" + y);
+        }
+    }
+}
