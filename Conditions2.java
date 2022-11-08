056d4bd163db88be6a3346ce8b2300a5495e6fc4
Rajpal Nishad <rajpal010304@gmail.com>
2022-11-08 20:24:10 +0530

    Short Hand If...Else

---
+++ Conditions2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Conditions2 {
+    public static void main(String[] args) {
+        // Short Hand If...Else
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age :");
+        int age = sc.nextInt();
+        /* Short hand if else compleate in one line */
+        String result = (age >= 18) ? "You are adult.\nYou can Vote." : "You are not adult.\nYou can not vote.";
+        System.out.println(result);
+    }
+}
